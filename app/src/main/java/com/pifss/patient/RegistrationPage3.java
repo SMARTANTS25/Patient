12c54d5ac80dd13fde1e6446448f9ab37cf58e7d
abraral-failakawi <ab.alf93@gmail.com>
2017-03-28 08:38:50 +0300

    new changes

---
+++ app/src/main/java/com/pifss/patient/RegistrationPage3.java
@@ -0,0 +1,13 @@
+package com.pifss.patient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationPage3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page3);
+    }
+}
