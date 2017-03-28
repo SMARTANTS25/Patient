3feb3f9214e74b8998ff2d5ede82624032ab32b9
macintosh <nwera_n94@outlook.com>
2017-03-28 10:23:15 +0300

    Hello its me

--- app/src/main/java/com/pifss/patient/RegistrationPage3.java
+++ app/src/main/java/com/pifss/patient/RegistrationPage3.java
@@ -9,5 +9,7 @@ public class RegistrationPage3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page3);
+
+        //hello its me
     }
 }
