f4bc4cd083e3e67509c108e461155986f9b8731e
SMARTANTS <alkhayat35@gmail.com>
2017-03-30 11:49:05 +0300

    View Hospital test2

--- app/src/main/java/com/pifss/patient/utils/Hospital.java
+++ app/src/main/java/com/pifss/patient/utils/Hospital.java
@@ -1,96 +1,32 @@
-package com.pifss.patient.utils;
+package com.pifss.patient;
 
-import com.activeandroid.Model;
-import com.activeandroid.annotation.Column;
-import com.activeandroid.annotation.Table;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * Created by SMARTANS on 3/27/2017.
- */
-@Table(name ="Hospital")
-public class Hospital extends Model {
+public class Hospital {
 
-    public Hospital(String hospitalName, String extraInfo, String email) {
+    private String address;
+    private String email;
+    private String extraInfo;
+    private String fax;
+    private String hospitalAlt;
+    private Integer hospitalId;
+    private String hospitalLang;
+    private String hospitalName;
+    private String logoUrl;
+    private String phone;
+    private Integer specialityId;
+    private String type;
+    private String webSite;
+    private String workingHours;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-
-        this.hospitalName = hospitalName;
-        this.extraInfo = extraInfo;
-        this.email = email;
+    public String getAddress() {
+        return address;
     }
 
-
-
-
-//     tvHospitalTitle.setText(hospital.getHospitalName());
-//        tvHospitalDescription.setText(hospital.getExtraInfo());
-//        tvHospitalEmail.setText(hospital.getEmail());
-    public Hospital(String email, String address, String extraInfo, String fax, String hospitalName, String phone, String webSite, String workingHours, String type, String hospitalLang, String hospitalAlt, String hospitalId) {
-        this.email = email;
+    public void setAddress(String address) {
         this.address = address;
-        this.extraInfo = extraInfo;
-        this.fax = fax;
-        this.hospitalName = hospitalName;
-        this.phone = phone;
-        this.webSite = webSite;
-        this.workingHours = workingHours;
-        this.type = type;
-        this.hospitalLang = hospitalLang;
-        this.hospitalAlt = hospitalAlt;
-        this.hospitalId = hospitalId;
-    }
-
-    @Column(name = "email")
-private
-String email;
-
-    @Column(name = "address")
-    private
-    String address;
-
-    @Column(name = "extraInfo")
-    private
-    String extraInfo;
-
-    @Column(name = "fax")
-    private
-    String fax;
-
-    @Column(name = "hospitalName")
-    private
-    String hospitalName;
-
-    @Column(name = "phone")
-    private
-    String phone;
-
-    @Column(name = "webSite")
-    private
-    String webSite;
-
-
-    @Column(name = "workingHours")
-    private
-    String workingHours;
-
-    @Column(name = "type")
-    private
-    String type;
-
-    @Column(name = "hospitalLang")
-    private
-    String hospitalLang;
-
-
-    @Column(name = "hospitalAlt")
-    private
-    String hospitalAlt;
-
-    @Column(name = "hospitalId")
-    private
-    String hospitalId;
-
-    public Hospital(String s) {
-
     }
 
     public String getEmail() {
@@ -99,14 +35,6 @@ String email;
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getExtraInfo() {
@@ -125,6 +53,30 @@ String email;
         this.fax = fax;
     }
 
+    public String getHospitalAlt() {
+        return hospitalAlt;
+    }
+
+    public void setHospitalAlt(String hospitalAlt) {
+        this.hospitalAlt = hospitalAlt;
+    }
+
+    public Integer getHospitalId() {
+        return hospitalId;
+    }
+
+    public void setHospitalId(Integer hospitalId) {
+        this.hospitalId = hospitalId;
+    }
+
+    public String getHospitalLang() {
+        return hospitalLang;
+    }
+
+    public void setHospitalLang(String hospitalLang) {
+        this.hospitalLang = hospitalLang;
+    }
+
     public String getHospitalName() {
         return hospitalName;
     }
@@ -133,12 +85,36 @@ String email;
         this.hospitalName = hospitalName;
     }
 
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
     public String getPhone() {
         return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Integer getSpecialityId() {
+        return specialityId;
+    }
+
+    public void setSpecialityId(Integer specialityId) {
+        this.specialityId = specialityId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getWebSite() {
@@ -157,53 +133,12 @@ String email;
         this.workingHours = workingHours;
     }
 
-    public String getType() {
-        return type;
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
     }
 
-    public String getHospitalLang() {
-        return hospitalLang;
-    }
-
-    public void setHospitalLang(String hospitalLang) {
-        this.hospitalLang = hospitalLang;
-    }
-
-    public String getHospitalAlt() {
-        return hospitalAlt;
-    }
-
-    public void setHospitalAlt(String hospitalAlt) {
-        this.hospitalAlt = hospitalAlt;
-    }
-
-    public String getHospitalId() {
-        return hospitalId;
-    }
-
-    public void setHospitalId(String hospitalId) {
-        this.hospitalId = hospitalId;
-    }
-
-//
-//    {
-//        "address": "bayan",
-//            "email": "a@whatever",
-//            "extraInfo": "a@whatever",
-//            "fax": "222",
-//            "hospitalAlt": "22.2",
-//            "hospitalId": 125,
-//            "hospitalLang": "1.23",
-//            "hospitalName": "test1",
-//            "logoUrl": "loho",
-//            "phone": "111",
-//            "specialityId": 33,
-//            "type": "mynon",
-//            "webSite": "web1",
-//            "workingHours": "24"
-//    }
 }
