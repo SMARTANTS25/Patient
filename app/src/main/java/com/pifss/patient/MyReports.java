faae147ebe579f970a5fa254886027ec39276570
SMARTANTS <alkhayat35@gmail.com>
2017-04-13 12:25:01 +0300

    Strings -->> Send Report

--- app/src/main/java/com/pifss/patient/MyReports.java
+++ app/src/main/java/com/pifss/patient/MyReports.java
@@ -1,6 +1,7 @@
 package com.pifss.patient;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,9 +17,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.pifss.patient.Adapters.DoctorAdapter;
 import com.pifss.patient.Adapters.ReportAdapter;
-import com.pifss.patient.utils.Doctor;
 import com.pifss.patient.utils.Reports;
 
 import org.json.JSONException;
@@ -41,7 +40,7 @@ public class MyReports extends AppCompatActivity {
         toolbar.setNavigationIcon(android.R.drawable.arrow_up_float);
         setSupportActionBar(toolbar);
 
-        String Patientid = getSharedPreferences("PatientData", MODE_PRIVATE).getString("Patient", " ");
+      //  String Patientid = getSharedPreferences("PatientData", MODE_PRIVATE).getString("Patient", " ");
 
 //        final ArrayList<Reports> parsedModel = new ArrayList<>();
 //
@@ -60,16 +59,14 @@ public class MyReports extends AppCompatActivity {
         updateModel ();
 
         lv = (ListView) findViewById(R.id.MyReportsLV);
-        ReportAdapter adapter = new ReportAdapter(model,this);
-
-        lv.setAdapter(adapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 Reports reports = model.get(position);
 
-                Intent intent = new Intent(MyReports.this,ReportDetails.class);
+                Intent intent = new Intent(MyReports.this, ReportDetails.class);
                 intent.putExtra("heartBeat",reports.getHeartbeatRate());
                 intent.putExtra("bloodPressure",reports.getBloodPressure());
                 intent.putExtra("drId",reports.getDrId());
@@ -85,6 +82,7 @@ public class MyReports extends AppCompatActivity {
                 intent.putExtra("patientId",reports.getPatientId());
                 intent.putExtra("drComment",reports.getDrcomment());
                 intent.putExtra("date",reports.getTimestamp());
+                System.out.print(reports.getPainlocation()+" "+reports.getDizziness());
                 Toast.makeText(MyReports.this, reports.getPainlocation()+" "+reports.getDizziness(), Toast.LENGTH_SHORT).show();
                 startActivity(intent);
             }
@@ -108,58 +106,40 @@ public class MyReports extends AppCompatActivity {
 
 
 
-    private void initAdapterWithFilter(String filter) {
-        if (model == null || model.size() <= 0) {
-            return;
-        }
-
-        final ArrayList<Doctor> parsedModel = new ArrayList<>();
-
-        for (int i = 0; i < model.size(); i++) {
-            Reports curDoc = model.get(i);
-            //String fullName = curDoc.getFirstName() + " "+ curDoc.getMiddleName() + " " + curDoc.getLastName();
-//            if ( fullName.toLowerCase().contains(filter) ) {
-//                parsedModel.add(curDoc);
+//    private void initAdapterWithFilter(String filter) {
+//        if (model == null || model.size() <= 0) {
+//            return;
+//        }
+//
+//
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//                Reports reports = model.get(position);
+//
+//                Intent intent = new Intent(MyReports.this,ReportDetails.class);
+//                intent.putExtra("heartBeat",reports.getHeartbeatRate());
+//                intent.putExtra("bloodPressure",reports.getBloodPressure());
+//                intent.putExtra("drId",reports.getDrId());
+//                intent.putExtra("comments",reports.getComments());
+//                intent.putExtra("fever",reports.getFever());
+//                intent.putExtra("coughing",reports.getCoughing());
+//                intent.putExtra("dizziness",reports.getDizziness());
+//                intent.putExtra("headache",reports.getHeadache());
+//                intent.putExtra("nauseous",reports.getNauseous());
+//                intent.putExtra("pain",reports.getPain());
+//                intent.putExtra("painLocation",reports.getPainlocation());
+//                intent.putExtra("SugarLever",reports.getSugarLevel());
+//                intent.putExtra("patientId",reports.getPatientId());
+//                intent.putExtra("drComment",reports.getDrcomment());
+//                intent.putExtra("date",reports.getTimestamp());
+//                Toast.makeText(MyReports.this, reports.getPainlocation()+" "+reports.getDizziness(), Toast.LENGTH_SHORT).show();
+//                startActivity(intent);
+//
 //            }
-        }
-
-
-        ListView lv = (ListView) findViewById(R.id.MyDoctorList);
-
-        DoctorAdapter adapter = new DoctorAdapter(parsedModel, this);
-
-
-        lv.setAdapter(adapter);
-
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                Reports reports = model.get(position);
-
-                Intent intent = new Intent(MyReports.this,ReportDetails.class);
-                intent.putExtra("heartBeat",reports.getHeartbeatRate());
-                intent.putExtra("bloodPressure",reports.getBloodPressure());
-                intent.putExtra("drId",reports.getDrId());
-                intent.putExtra("comments",reports.getComments());
-                intent.putExtra("fever",reports.getFever());
-                intent.putExtra("coughing",reports.getCoughing());
-                intent.putExtra("dizziness",reports.getDizziness());
-                intent.putExtra("headache",reports.getHeadache());
-                intent.putExtra("nauseous",reports.getNauseous());
-                intent.putExtra("pain",reports.getPain());
-                intent.putExtra("painLocation",reports.getPainlocation());
-                intent.putExtra("SugarLever",reports.getSugarLevel());
-                intent.putExtra("patientId",reports.getPatientId());
-                intent.putExtra("drComment",reports.getDrcomment());
-                intent.putExtra("date",reports.getTimestamp());
-                Toast.makeText(MyReports.this, reports.getPainlocation()+" "+reports.getDizziness(), Toast.LENGTH_SHORT).show();
-                startActivity(intent);
-
-            }
-        });
-    }
+//        });
+//    }
 
 
 
@@ -167,56 +147,61 @@ public class MyReports extends AppCompatActivity {
 
     private void updateModel () {
 
-        String Pid="";
-        String PatientDat = getSharedPreferences("PatientData1",MODE_PRIVATE).getString("Patient1"," ");
-        try {
-            JSONObject o = new JSONObject(PatientDat);
-            Pid = o.getString("patientId");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+//        String Pid="";
+//        String PatientDat = getSharedPreferences("PatientData1",MODE_PRIVATE).getString("Patient1"," ");
+//        try {
+//            JSONObject o = new JSONObject(PatientDat);
+//            Pid = o.getString("patientId");
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
         //  Toast.makeText(this, Pid+"", Toast.LENGTH_SHORT).show();
-        String url = "http://34.196.107.188:8081/MhealthWeb/webresources/patientreport/"+Pid;
+        String url = "http://34.196.107.188:8081/MhealthWeb/webresources/patientreport/";
 
         final RequestQueue queue= MySingleton.getInstance().getRequestQueue(MyReports.this);
 
-        final StringRequest jsonReq = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-//                        SharedPreferences sharedpreferences = getSharedPreferences("MyReportData", MODE_PRIVATE);
-//
-//                        System.out.println(response);
-//
-//                        try {
-//                            JSONObject obj = new JSONObject(response);
-//                            sharedpreferences.edit()
-//                                    .putString("MyReport" , obj.toString())
-//                                    .commit();
-//
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-
-                        model = new Gson().fromJson(response.toString(), new TypeToken<ArrayList<Doctor>>(){}.getType());
-
-                         ReportAdapter adapter = new ReportAdapter(model, MyReports.this);
-
-                        lv.setAdapter(adapter);
 
 
 
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Handle error
-                        Toast.makeText(MyReports.this, "Connection error sorry", Toast.LENGTH_SHORT).show();
-                    }
-                });
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
-        queue.add(jsonReq);
+
+
+                SharedPreferences sharedpreferences = getSharedPreferences("MyReportData", MODE_PRIVATE);
+
+                System.out.println(response);
+
+                sharedpreferences.edit()
+                        .putString("MyReport" , response)
+                        .commit();
+                Gson gson = new Gson();
+                JSONObject json = null;
+                TypeToken <ArrayList<Reports>> token= new TypeToken<ArrayList<Reports>>(){};
+                try {
+                    json = new JSONObject(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                model = gson.fromJson(response,token.getType());
+
+                ReportAdapter adapter = new ReportAdapter(model, MyReports.this);
+
+                lv.setAdapter(adapter);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MyReports.this, error.getMessage(), Toast.LENGTH_LONG);
+            }
+        });
+        System.out.println(model);
+        //System.out.println(model.size());
+        Toast.makeText(this, "reached here ", Toast.LENGTH_SHORT).show();
+
+        queue.add(request);
     }
 }
